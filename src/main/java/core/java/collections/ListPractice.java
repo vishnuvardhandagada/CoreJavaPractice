1e2a50f6bda5fd8ef0c2be4af7217bcb7b6e8f5b
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2016-12-21 08:50:55 +0530

    List to array
    List to array

--- src/main/java/core/java/collections/ListPractice.java
+++ src/main/java/core/java/collections/ListPractice.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ListPractice {
@@ -25,11 +26,46 @@ public class ListPractice {
 	return splits;
     }
 
-    //    @Ignore
+    @Ignore
     @Test
     public void convertArrayToList() {
-	int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	List<Integer> numbersList = Arrays.stream(numbersArray).boxed().collect(Collectors.toList());
-	System.out.println(numbersList);
+	String[] stringArray = new String[] { "A", "B", "C", "D" };
+	List<String> stringList = Arrays.asList(stringArray);
+	System.out.println("stringList: " + stringList);
+
+	List<String> stringList2 = new ArrayList<>(Arrays.asList(stringArray));
+	System.out.println("stringList2: " + stringList2);
+
+	int[] numbersArray1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	List<Integer> numbersList1 = new ArrayList<>();
+	for (int number : numbersArray1) {
+	    numbersList1.add(number);
+	}
+	System.out.println("numbersList1: " + numbersList1);
+
+	// from java 8
+	int[] numbersArray2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	List<Integer> numbersList2 = Arrays.stream(numbersArray2).boxed().collect(Collectors.toList());
+	System.out.println("numbersList2: " + numbersList2);
+
+	Integer[] numbersArray3 = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	List<Integer> numbersList3 = Arrays.asList(numbersArray3);
+	System.out.println("numbersList3: " + numbersList3);
+
+    }
+
+    @Ignore
+    @Test
+    public void convertListToArray() {
+	List<String> list1 = new ArrayList<>();
+	list1.add("A");
+	list1.add("B");
+	list1.add("C");
+	System.out.println("list1: " + list1);
+
+	String[] list1Array = list1.toArray(new String[list1.size()]);
+	for (String value : list1Array) {
+	    System.out.print(value + ",");
+	}
     }
 }
