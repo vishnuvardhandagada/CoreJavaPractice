5b7500751ff09027c0858c1b2df2e05634d0952a
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2017-11-21 10:51:29 +0530

    Exception Handling

--- src/main/java/core/java/ExceptionHandlingPractice.java
+++ src/main/java/core/java/ExceptionHandlingPractice.java
@@ -4,16 +4,44 @@ import org.junit.Test;
 
 public class ExceptionHandlingPractice {
 
-    @Test
-    public void exceptionInForLoop() {
-	try {
-	    for (int i = 0; i < 10; i++) {
-		if (i == 5)
-		    throw new NullPointerException("Exception thrown");
-	    }
-	} catch (NullPointerException e) {
-	    e.printStackTrace();
+	@Test
+	public void exceptionInForLoop() {
+		try {
+			for (int i = 0; i < 10; i++) {
+				if (i == 5)
+					throw new NullPointerException("Exception thrown");
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-    }
+	/**
+	 * RuntimeException/unchecked exception
+	 */
+	@Test
+	public void uncheckedException() {
+		throw new RuntimeException("RuntimeException thrown");
+	}
+
+	/**
+	 * Exception/checked exception
+	 */
+	@Test
+	public void checkedException() throws Exception {
+		throw new Exception("Exception object created and thrown");
+	}
+
+	/**
+	 * Handle runtime/unchecked exception using try-catch
+	 */
+	@Test
+	public void uncheckedWithTryCatch() {
+		try {
+			throw new RuntimeException("RuntimeException thrown");
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
 }
